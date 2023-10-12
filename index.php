56ea0129c0c56f60506c8a8b1f1e3a74b2d56912
Gameplushy <victor_f@orange.fr>
2023-10-12 12:58:43 +0200

    Use WebSocket on webpage

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
     <head>
         <title>PixelWar WebSocket testing</title>
         <script>
-            function hello(){
-                console.log("Hi!")
+            var conn = new WebSocket('ws://localhost:8080');
+            conn.onopen = function(e) {
+                console.log("Connection established!");
+            };
+
+            conn.onmessage = function(e) {
+                console.log(e.data);
             }
+
+            function hello(){
+                conn.send("Hi!");
+            }
+
         </script>
     </head>
     <body>
